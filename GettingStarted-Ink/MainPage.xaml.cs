62fbaf6d4bcc2946d673301c615d3061e2104ae8
Travis Menghini <menghinitravis@gmail.com>
2020-04-09 11:09:36 -0500

    Added ink support.

--- GettingStarted-Ink/MainPage.xaml.cs
+++ GettingStarted-Ink/MainPage.xaml.cs
@@ -28,12 +28,12 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 
-// Begin "Step 2: Use InkCanvas to support basic inking"
-////using directives for inking functionality.
-//using Windows.UI.Input.Inking;
-//using Windows.UI.Input.Inking.Analysis;
-//using Windows.UI.Xaml.Shapes;
-//using Windows.Storage.Streams;
+//Begin "Step 2: Use InkCanvas to support basic inking"
+//using directives for inking functionality.
+using Windows.UI.Input.Inking;
+using Windows.UI.Input.Inking.Analysis;
+using Windows.UI.Xaml.Shapes;
+using Windows.Storage.Streams;
 // End "Step 2: Use InkCanvas to support basic inking"
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
